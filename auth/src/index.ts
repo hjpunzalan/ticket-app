8dda94e46b136f7d32b5bc3c6f9869ac9f9235f1
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-29 22:05:10 +0800

    Defining new custom errors ex. not found page

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/errorHandler";
+import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
 app.use(json());
@@ -14,6 +15,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", () => {
+	throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
