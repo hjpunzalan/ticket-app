a878fa457a4c3dda8f0ee02f53a132b6f73658a1
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-29 17:44:39 +0800

    Extending error class and fixing bug with prototype chain

---
+++ auth/src/errors/requestValidationErrors.ts
@@ -0,0 +1,10 @@
+import { ValidationError } from "express-validator";
+
+export class RequestValidationErorr extends Error {
+	constructor(private errors: ValidationError[]) {
+		super(); // 'Error' breaks prototype chain here due to Typescript converting to ES2015
+
+		// Only because we are extending a built in class
+		Object.setPrototypeOf(this, RequestValidationErorr.prototype); // restore prototype chain
+	}
+}
