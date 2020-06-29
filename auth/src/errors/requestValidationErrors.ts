ecfd4c0e4a183a35ba8fd36540e830c6be5d41ee
hjpunzalan <hj.punzalan@hotmail.com>
2020-06-29 21:13:56 +0800

    Moving logic from handler to the error class

--- auth/src/errors/requestValidationErrors.ts
+++ auth/src/errors/requestValidationErrors.ts
@@ -1,10 +1,18 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationErorr extends Error {
+	statusCode = 400;
+
 	constructor(public errors: ValidationError[]) {
 		super(); // 'Error' breaks prototype chain here due to Typescript converting to ES2015
 
 		// Only because we are extending a built in class
 		Object.setPrototypeOf(this, RequestValidationErorr.prototype); // restore prototype chain
+	}
+
+	serializeErrors() {
+		return this.errors.map((error) => {
+			return { message: error.msg, field: error.param };
+		});
 	}
 }
