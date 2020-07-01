d43d66f3dd49d4a335588cbf76b74d7e9e501cde
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-01 17:44:55 +0800

    User model and build user function

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
-import { currentUserRouter } from "./routes/GFcurrentUser";
+import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
@@ -23,6 +24,21 @@ app.all("*", () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-	console.log("Listening on port 3000!!");
-});
+const start = async () => {
+	try {
+		// Mongodb will create a database if we connect it to a new one
+		await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.error(error);
+	}
+	app.listen(3000, () => {
+		console.log("Listening on port 3000!!!");
+	});
+};
+
+start();
