691960657eb5d827eeec9ac81c33a9056cca9b9b
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-01 21:03:20 +0800

    Type Checking user doc,model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -2,8 +2,20 @@ import mongoose from "mongoose";
 
 // An interface that describes the properties
 // that are required to create a new user
-
 interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+// An interface that describes the properties
+// a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
 	email: string;
 	password: string;
 }
@@ -19,12 +31,10 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
-const User = mongoose.model("User", userSchema);
-
-// Checks types of properties before compilation
-//
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
 	return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
