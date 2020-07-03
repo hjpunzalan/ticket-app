18a7b59a307ce14545b6b12ea1a9450a6a5b1779
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-03 18:01:17 +0800

    Generating a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import { RequestValidationErorr } from "./../errors/requestValidationErrors";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/badRequestError";
@@ -32,6 +33,20 @@ router.post(
 
 		const user = User.build({ email, password });
 		await user.save();
+
+		// Generate JWT
+		const userJwt = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+			},
+			"asdf"
+		);
+
+		// Store in session object
+		req.session = {
+			jwt: userJwt,
+		};
 
 		res.status(201).send(user);
 	}
