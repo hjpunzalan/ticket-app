816d9b0843a8ff7e29435c26bb8d406cf1c4be5e
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-11 20:24:44 +0800

    Auth helper test function

---
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { app } from "../../app";
+
+test("responds with details about the current user", async () => {
+	const cookie = await global.signin();
+
+	const response = await request(app)
+		.get("/api/users/currentuser")
+		.set("Cookie", cookie)
+		.expect(200);
+	expect(response.body.currentUser.email).toEqual("test@test.com");
+});
