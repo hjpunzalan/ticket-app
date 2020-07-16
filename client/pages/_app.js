0302496b3e5558cb20739139abbf44d7bc49f5bb
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-17 03:07:18 +0800

    signup form

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,4 @@
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css"; // can only import global css here at app file
 
 export default ({ Component, pageProps }) => {
 	return <Component {...pageProps} />;
