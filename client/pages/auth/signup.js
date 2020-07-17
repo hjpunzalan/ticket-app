1c160f3d494c50e416cfaf91b4a68df8c16cf33d
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-18 00:04:04 +0800

    Add onSuccess Callback for navigation

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Router from "next/router";
 import useRequest from "../../hooks/useRequest";
 
 export default () => {
@@ -8,6 +9,7 @@ export default () => {
 		url: "/api/users/signup",
 		method: "post",
 		body: { email, password },
+		onSuccess: () => Router.push("/"),
 	});
 
 	const onSubmit = (e) => {
