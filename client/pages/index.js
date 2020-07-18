304b56df87da7509a1d69e4336e49c626c958224
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-18 23:44:08 +0800

    attempt to add getinitialProps to index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,18 @@
-export default () => {
-	return <h1>Landing page</h1>;
+import axios from "axios";
+
+/* Dont use hooks outside of react components */
+const LandingPage = ({ currentUser }) => {
+	console.log(currentUser);
+	return <div>Landing Page</div>;
 };
+
+// Making request from server
+LandingPage.getInitialProps = async () => {
+	const response = await axios.get(
+		"http://ingress-nginx-controller-admission.kube-system.svc.cluster.local/api/users/currentuser"
+	);
+
+	return response.data;
+};
+
+export default LandingPage;
