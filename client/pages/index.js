f06ee26e67234de283cf7fc8d0ade243a01b6585
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-19 23:00:53 +0800

    Specifying host and passing through cookies/headers

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,11 +10,18 @@ const LandingPage = ({ currentUser }) => {
 LandingPage.getInitialProps = async ({ req }) => {
 	let baseUrl = "";
 	if (typeof window === "undefined") {
+		// in the server otherwise if window !== undefined, it will be called from browser
+
 		// x-forwarded-proto set by nginx, IP address for headers.host mapped in pod config
 		// replace http with ${req.headers["x-forwarded-proto"]}
+
+		// From the client pod, send request to the ingress nginx domain
+		// the client pod states that its host domain is the ingress nginx domain
 		baseUrl = `http://${req.headers.host}`;
 	}
-	const { data } = await axios.get(`${baseUrl}/api/users/currentuser`);
+	const { data } = await axios.get(`${baseUrl}/api/users/currentuser`, {
+		headers: req.headers,
+	});
 	return data;
 };
 
