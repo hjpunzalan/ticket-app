3d44cd559fcdd50598e762c882772e827673cd1c
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-19 17:12:40 +0800

    Fixing the baseUrl of landing page intial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,14 +6,13 @@ const LandingPage = ({ currentUser }) => {
 	return <div>Landing Page</div>;
 };
 
-// Making request from server
+// Making request from server unless navigating between pages within the app
 LandingPage.getInitialProps = async ({ req }) => {
 	let baseUrl = "";
-	console.log(`${req["x-forwarded-proto"]}://${req.headers.host}`);
-
 	if (typeof window === "undefined") {
 		// x-forwarded-proto set by nginx, IP address for headers.host mapped in pod config
-		baseUrl = `${req["x-forwarded-proto"]}://${req.headers.host}`;
+		// replace http with ${req.headers["x-forwarded-proto"]}
+		baseUrl = `http://${req.headers.host}`;
 	}
 	const { data } = await axios.get(`${baseUrl}/api/users/currentuser`);
 	return data;
