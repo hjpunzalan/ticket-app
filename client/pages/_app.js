c77330ab67593a4214ea0c8542e7b9d2048412c7
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-20 23:57:08 +0800

    get initialProps at app component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,14 @@
 import "bootstrap/dist/css/bootstrap.css"; // can only import global css here at app file
+import buildClient from "../api/buildClient";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
 	return <Component {...pageProps} />;
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+	const client = buildClient(appContext.ctx);
+	const { data } = await client.get("/api/users/currentuser");
+	return data;
+};
+
+export default AppComponent;
