b5a99b02124bf817dab10882681edb5b88bc8e2c
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-20 22:28:18 +0800

    Reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/buildClient";
 
 /* Dont use hooks outside of react components */
 const LandingPage = ({ currentUser }) => {
@@ -7,21 +7,9 @@ const LandingPage = ({ currentUser }) => {
 };
 
 // Making request from server unless navigating between pages within the app
-LandingPage.getInitialProps = async ({ req }) => {
-	let baseUrl = "";
-	if (typeof window === "undefined") {
-		// in the server otherwise if window !== undefined, it will be called from browser
-
-		// x-forwarded-proto set by nginx, IP address for headers.host mapped in pod config
-		// replace http with ${req.headers["x-forwarded-proto"]}
-
-		// From the client pod, send request to the ingress nginx domain
-		// the client pod states that its host domain is the ingress nginx domain
-		baseUrl = `http://${req.headers.host}`;
-	}
-	const { data } = await axios.get(`${baseUrl}/api/users/currentuser`, {
-		headers: req.headers,
-	});
+LandingPage.getInitialProps = async (context) => {
+	const client = buildClient(context);
+	const { data } = await client.get("/api/users/currentuser");
 	return data;
 };
 
