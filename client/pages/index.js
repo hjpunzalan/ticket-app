5a4a7dc4b014711b9a2f404fb6aefe9c61ff0972
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-20 22:29:43 +0800

    Context on Landing Page

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,8 +2,11 @@ import buildClient from "../api/buildClient";
 
 /* Dont use hooks outside of react components */
 const LandingPage = ({ currentUser }) => {
-	console.log(currentUser);
-	return <div>Landing Page</div>;
+	return currentUser ? (
+		<h1>You are signed in</h1>
+	) : (
+		<h1>You are NOT signed in</h1>
+	);
 };
 
 // Making request from server unless navigating between pages within the app
