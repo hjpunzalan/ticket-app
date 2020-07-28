fd00adb4170099e0604df0ab13ad81efe16ad4a6
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-28 20:31:05 +0800

    Building a session

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,8 +9,22 @@ test("has a route handler listening to /api/tickets for post requests", async ()
 	expect(response.status).not.toEqual(404);
 });
 
-test("can only be access if the user is signed in", () => {});
+test("can only be access if the user is signed in", async () => {
+	const response = await request(app)
+		.post("/api/tickets")
+		.send({})
+		.expect(401);
+});
 
-test("returns an error if an invalid title is provided", () => {});
+test("returns a status other than 401 if the user is signed in", async () => {
+	const response = await request(app)
+		.post("/api/tickets")
+		.set("Cookie", global.signin())
+		.send({});
 
-test("creates a ticket with valid inputs", () => {});
+	expect(response.status).not.toEqual(401);
+});
+
+test("returns an error if an invalid title is provided", async () => {});
+
+test("creates a ticket with valid inputs", async () => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import { app } from "../app";
-import request from "supertest";
+import jwt from "jsonwebtoken";
 
 declare global {
 	namespace NodeJS {
 		interface Global {
-			signin(): Promise<string[]>;
+			signin(): string[];
 		}
 	}
 }
@@ -38,19 +37,23 @@ afterAll(async () => {
 });
 
 // global scoped function for ease
-global.signin = async () => {
-	const email = "test@test.com";
-	const password = "password";
+global.signin = () => {
+	// Build a JWT payload. { id, email}
+	const payload = {
+		id: "12345",
+		email: "test@test.com",
+	};
 
-	const response = await request(app)
-		.post("/api/users/signup")
-		.send({
-			email,
-			password,
-		})
-		.expect(201);
+	// Create a JWT
+	const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-	const cookie = response.get("Set-Cookie");
+	// Build a session.object. { jwt: MY_JWT}
+	const session = { jwt: token };
+	// Turn that session into JSON
+	const sessionJSON = JSON.stringify(session);
+	// Take JSON and encode it as base64
+	const base64 = Buffer.from(sessionJSON).toString("base64");
 
-	return cookie;
+	//return a string thats the cookie with the encoded data
+	return [`express:sess=${base64}`];
 };
