b20e08977669dc21e9f33e6047b94fd9485d4407
hjpunzalan <hj.punzalan@hotmail.com>
2020-07-31 11:34:16 +0800

    Better error logging

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,12 @@
 import request from "supertest";
 import { app } from "../../app";
+import mongoose from "mongoose";
 
 test("returns a 404 if the ticket is not found", async () => {
+	const id = new mongoose.Types.ObjectId().toHexString();
+
 	await request(app)
-		.get("/api/tickets/fakeid")
+		.get(`/api/tickets/${id}`)
 		.send()
 		.expect(404);
 });
