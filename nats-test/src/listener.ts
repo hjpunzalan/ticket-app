43e100ee8d2e060d7dfb01b583cd56057d4c73d3
Jonathan <hj.punzalan@hotmail.com>
2020-08-10 18:16:22 +0800

    Manual ack mode

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,11 +10,17 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
 	console.log("Listener connected to nats");
 
+	// Set manual ack mode true
+	// event is only completed only once acknowledged otherwise sent again
+	// useful in case listeners goes down etc.
+	const options = stan.subscriptionOptions().setManualAckMode(true);
+
 	//esablish listener to ticket:created channel
 	// add into a queue group
 	const subscription = stan.subscribe(
 		"ticket:created",
-		"orders-service-queue-group"
+		"orders-service-queue-group",
+		options
 	);
 
 	// listen to any published message
@@ -24,5 +30,6 @@ stan.on("connect", () => {
 		if (typeof data === "string") {
 			console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
 		}
+		msg.ack();
 	});
 });
