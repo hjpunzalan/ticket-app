afbb97256dfd9338f2ff4a3646df0e1737fdce09
Jonathan <hj.punzalan@hotmail.com>
2020-08-13 23:54:53 +0800

    added package descriptions for each service

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -6,7 +6,6 @@ console.clear;
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 	url: "http://localhost:4222",
 });
-
 stan.on("connect", () => {
 	console.log("Listener connected to nats");
 
