39f7e85ee2e06e19ef2a7d2a517cb1c917f5b275
Jonathan <hj.punzalan@hotmail.com>
2020-08-18 21:20:05 +0800

    Redirecting import for mocking nats-wrapper - testing

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,19 +2,16 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 
+jest.mock("../../nats-wrapper");
+
 test("has a route handler listening to /api/tickets for post requests", async () => {
-	const response = await request(app)
-		.post("/api/tickets")
-		.send({});
+	const response = await request(app).post("/api/tickets").send({});
 
 	expect(response.status).not.toEqual(404);
 });
 
 test("can only be access if the user is signed in", async () => {
-	const response = await request(app)
-		.post("/api/tickets")
-		.send({})
-		.expect(401);
+	const response = await request(app).post("/api/tickets").send({}).expect(401);
 });
 
 test("returns a status other than 401 if the user is signed in", async () => {
