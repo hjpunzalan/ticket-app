29695833b7770233036d91e028887e6b25d94046
Jonathan <hj.punzalan@hotmail.com>
2020-08-25 21:56:55 +0800

    test for ticket not existing order creation

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,36 +4,57 @@ import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { natsWrapper } from "./../../nats-wrapper";
 
-test("has a route handler listening to /api/orders for post requests", async () => {
-	const response = await request(app).post("/api/orders").send({});
-	expect(response.status).not.toEqual(404);
+describe("Request validation", () => {
+	test("has a route handler listening to /api/orders for post requests", async () => {
+		const response = await request(app).post("/api/orders").send({});
+		expect(response.status).not.toEqual(404);
+	});
+
+	test("can only be accessed if the user is signed in", async () => {
+		const response = await request(app)
+			.post("/api/orders")
+			.send({})
+			.expect(401);
+	});
+
+	test("returns a status other than 401 if the user is signed in", async () => {
+		const response = await request(app)
+			.post("/api/orders")
+			.set("Cookie", global.signin())
+			.send({});
+
+		expect(response.status).not.toEqual(401);
+	});
+
+	test("returns an error if an invalid ticketId is provided", async () => {
+		await request(app)
+			.post("/api/orders")
+			.set("Cookie", global.signin())
+			.send({ ticketId: "" })
+			.expect(400);
+
+		// if valid ticket is provided should pass
+		const id = new mongoose.Types.ObjectId().toHexString();
+		await request(app)
+			.post("/api/orders")
+			.set("Cookie", global.signin())
+			.send({ ticketId: id })
+			.expect(404);
+	});
 });
 
-test("can only be accessed if the user is signed in", async () => {
-	const response = await request(app).post("/api/orders").send({}).expect(401);
-});
-
-test("returns a status other than 401 if the user is signed in", async () => {
-	const response = await request(app)
-		.post("/api/orders")
-		.set("Cookie", global.signin())
-		.send({});
-
-	expect(response.status).not.toEqual(401);
-});
-
-test("returns an error if an invalid ticketId is provided", async () => {
-	await request(app)
-		.post("/api/orders")
-		.set("Cookie", global.signin())
-		.send({ ticketId: "" })
-		.expect(400);
-
-	// if valid ticket is provided should pass
-	const id = new mongoose.Types.ObjectId().toHexString();
-	await request(app)
-		.post("/api/tickets")
-		.set("Cookie", global.signin())
-		.send({ ticketId: id })
-		.expect(404);
+describe("Order creation", () => {
+	test("returns an error if the ticket does not exist", async () => {
+		// if valid ticket is provided should pass
+		const id = new mongoose.Types.ObjectId().toHexString();
+		await request(app)
+			.post("/api/orders")
+			.set("Cookie", global.signin())
+			.send({
+				ticketId: id,
+			})
+			.expect(404);
+	});
+	test("returns an error if the ticket is already reserved", () => {});
+	test("reserves a ticket", () => {});
 });
