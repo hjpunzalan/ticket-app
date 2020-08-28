096fc665b43e0074bdc9c47888eaccc2e897a55e
Jonathan <hj.punzalan@hotmail.com>
2020-08-28 18:06:39 +0800

    Fetching individual orders

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import {
+	requireAuth,
+	BadRequestError,
+	NotFoundError,
+	NotAuthorizedError,
+} from "@hjtickets/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-	res.send({});
-});
+router.get(
+	"/api/orders/:orderId",
+	requireAuth,
+	async (req: Request, res: Response) => {
+		// Check if order Id is valid
+		if (!mongoose.Types.ObjectId.isValid(req.params.orderId)) {
+			throw new BadRequestError("Order id is invalid.");
+		}
+
+		const order = await Order.findById(req.params.orderId).populate("ticket");
+		if (!order) {
+			throw new NotFoundError();
+		}
+
+		if (order.userId !== req.currentUser!.id) {
+			throw new NotAuthorizedError();
+		}
+
+		res.send({});
+	}
+);
 
 export { router as showOrderRouter };
