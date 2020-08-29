669236ed5e07b48de4593e73e7c5ab6cb012845a
Jonathan <hj.punzalan@hotmail.com>
2020-08-29 18:02:46 +0800

    Cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,39 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import {
+	requireAuth,
+	BadRequestError,
+	NotFoundError,
+	NotAuthorizedError,
+	OrderStatus,
+} from "@hjtickets/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-	res.send({});
-});
+router.delete(
+	"/api/orders/:orderId",
+	requireAuth,
+	async (req: Request, res: Response) => {
+		// Check if order Id is valid
+		if (!mongoose.Types.ObjectId.isValid(req.params.orderId)) {
+			throw new BadRequestError("Order id is invalid.");
+		}
+
+		const order = await Order.findById(req.params.orderId);
+		if (!order) {
+			throw new NotFoundError();
+		}
+
+		if (order.userId !== req.currentUser!.id) {
+			throw new NotAuthorizedError();
+		}
+
+		order.status = OrderStatus.Cancelled;
+		await order.save();
+
+		res.status(204).send(order);
+	}
+);
 
 export { router as deleteOrderRouter };
