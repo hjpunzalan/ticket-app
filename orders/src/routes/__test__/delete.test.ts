a48f3ab671279033281e60074d78a86170932ff5
Jonathan <hj.punzalan@hotmail.com>
2020-08-30 18:12:02 +0800

    todo : publish delete  order event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
 
 test("should find the order by order id and delete it", async () => {
 	// Create the ticket
@@ -54,4 +55,32 @@ test("returns an error if one user tries to fetch another user's order", async (
 		.expect(401);
 });
 
-test("marks an order as cancelled", async () => {});
+test("marks an order as cancelled", async () => {
+	// create a ticket with the Ticket model
+	const ticket = Ticket.build({
+		title: "concert",
+		price: 20,
+	});
+	await ticket.save();
+
+	// make a request to create an order
+	const user = global.signin();
+	const { body: order } = await request(app)
+		.post("/api/orders")
+		.set("Cookie", user)
+		.send({ ticketId: ticket.id })
+		.expect(201);
+
+	// make a request to cancel the order
+	await request(app)
+		.delete(`/api/orders/${order.id}`)
+		.set("Cookie", user)
+		.send()
+		.expect(200);
+
+	// expectation to make sure the thing is cancelled
+	const updatedOrder = await Order.findById(order.id);
+	expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+test.todo("emits an order cancelled event");
