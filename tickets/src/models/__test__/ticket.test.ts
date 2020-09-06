fec940d29e1c4d1998eb91d8159a7826e298e944
hjpunzalan <hj.punzalan@hotmail.com>
2020-09-06 22:34:57 +0800

    Testing OCC

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,3 +1,33 @@
 import { Ticket } from "../ticket";
 
-test("implements optimistic concurrency control", async () => {});
+test("implements optimistic concurrency control", async (done) => {
+	// Create an instance of a ticket
+	const ticket = Ticket.build({
+		title: "concert",
+		price: 5,
+		userId: "123",
+	});
+
+	// Save the ticket to the database'
+	await ticket.save();
+
+	// Fetch the ticket twice
+	const firstInstance = await Ticket.findById(ticket.id);
+	const secondInstance = await Ticket.findById(ticket.id);
+
+	// Make two separate changes to the tickets we fetched
+	firstInstance!.set({ price: 10 });
+	secondInstance!.set({ price: 15 });
+
+	// save the first fetched ticket
+	await firstInstance!.save();
+
+	// save the second fetched ticket (outdated version number) and expect an error
+	try {
+		await secondInstance!.save();
+	} catch (error) {
+		return done();
+	}
+
+	throw new Error("Should not reach this point");
+});
