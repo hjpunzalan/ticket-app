266e644fced272f792986b1fccd8f68f59d15084
hjpunzalan <hj.punzalan@hotmail.com>
2020-09-09 23:10:03 +0800

    abstract findbyEvent to ticket model - order service

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
 	build(attrs: TicketAttrs): TicketDoc;
+	findByEvent(event: {
+		id: string;
+		version: number;
+	}): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -42,13 +46,19 @@ const ticketSchema = new mongoose.Schema(
 );
 ticketSchema.set("versionKey", "version");
 ticketSchema.plugin(updateIfCurrentPlugin);
-
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
 	return new Ticket({
 		_id: attrs.id,
 		...attrs,
 	});
 };
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+	return Ticket.findOne({
+		_id: event.id,
+		version: event.version - 1,
+	});
+};
+
 ticketSchema.methods.isReserved = async function () {
 	const existingOrder = await Order.findOne({
 		ticket: this,
