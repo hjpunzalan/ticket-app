54ebd76dfe01f523e3723998afb4c608f60a1ea2
hjpunzalan <hj.punzalan@hotmail.com>
2020-09-27 18:14:58 +0800

    Ticket creation onSubmit

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,8 +1,23 @@
 import { useState } from "react";
+import useRequest from "../../hooks/useRequest";
 
 const NewTicket = () => {
 	const [title, setTitle] = useState("");
 	const [price, setPrice] = useState("");
+	const { doRequest, errors } = useRequest({
+		url: "/api/tickets",
+		method: "post",
+		body: {
+			title,
+			price,
+		},
+		onSuccess: (ticket) => console.log(ticket),
+	});
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		doRequest();
+	};
 
 	// Clicking in and out of input event handler
 	const onBlur = () => {
@@ -15,7 +30,7 @@ const NewTicket = () => {
 	return (
 		<div>
 			<h1>Create a ticket</h1>
-			<form>
+			<form onSubmit={onSubmit}>
 				<div className="form-group">
 					<label>Title</label>
 					<input
@@ -33,6 +48,7 @@ const NewTicket = () => {
 						className="form-control"
 					/>
 				</div>
+				{errors}
 				<button className="btn btn-primary">Submit</button>
 			</form>
 		</div>
