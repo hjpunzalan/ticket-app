20a681b091d87fc91b2f78613d6dffc8c16f2621
hjpunzalan <hj.punzalan@hotmail.com>
2020-09-27 18:05:14 +0800

    create new ticket - onBlur event handler

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,37 @@
-const newTicket = () => {
+import { useState } from "react";
+
+const NewTicket = () => {
+	const [title, setTitle] = useState("");
+	const [price, setPrice] = useState("");
+
+	// Clicking in and out of input event handler
+	const onBlur = () => {
+		const value = parseFloat(price);
+
+		if (isNaN(value)) return;
+		setPrice(value.toFixed(2));
+	};
+
 	return (
 		<div>
 			<h1>Create a ticket</h1>
 			<form>
 				<div className="form-group">
-					<label className="form-control">Title</label>
-					<input />
+					<label>Title</label>
+					<input
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						className="form-control"
+					/>
 				</div>
 				<div className="form-group">
 					<label>Price</label>
-					<input className="form-control" />
+					<input
+						onBlur={onBlur}
+						value={price}
+						onChange={(e) => setPrice(e.target.value)}
+						className="form-control"
+					/>
 				</div>
 				<button className="btn btn-primary">Submit</button>
 			</form>
@@ -17,4 +39,4 @@ const newTicket = () => {
 	);
 };
 
-export default newTicket;
+export default NewTicket;
