659cd95a8006481cb95b1035e991009151776865
hjpunzalan <hj.punzalan@hotmail.com>
2020-10-14 17:22:15 +0800

    feat: Filtering reserved tickets

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -4,7 +4,10 @@ import { Ticket } from "../models/ticket";
 const router = express.Router();
 
 router.get("/api/tickets", async (req: Request, res: Response) => {
-	const tickets = await Ticket.find({});
+	// only find tickets that have not been ordered
+	const tickets = await Ticket.find({
+		orderId: undefined,
+	});
 	res.send(tickets);
 });
 
