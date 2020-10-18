169cc4b298a9d6b6cb6287d75fe09612d1dc1755
hjpunzalan <hj.punzalan@hotmail.com>
2020-10-18 18:09:20 +0800

    fix: removed starting message for auth and order service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,7 +7,6 @@ import { TicketCreatedListener } from "./events/listeners/ticket-created-listene
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete.listener";
 
 const start = async () => {
-	console.log("Starting......");
 	if (!process.env.JWT_KEY) {
 		throw new Error("JWT_KEY must be defined");
 	}
